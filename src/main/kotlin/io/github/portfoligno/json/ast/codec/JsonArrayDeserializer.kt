749ad8632fa803d69021bf23b8398ee61943f3eb
Nicholas Yip <nicholasyip@obceit.cc>
2019-02-05 02:05:59 +0900

    Simplify while loops

--- src/main/kotlin/io/github/portfoligno/json/ast/codec/JsonArrayDeserializer.kt
+++ src/main/kotlin/io/github/portfoligno/json/ast/codec/JsonArrayDeserializer.kt
@@ -12,22 +12,20 @@ object JsonArrayDeserializer : ExpectedTokenDeserializer<JsonArray>(START_ARRAY)
   fun invoke(p: JsonParser, context: DeserializationContext): JsonArray {
     val elements = ImmutableList.builder<Json>()
 
-    apply loop@{
-      while (true) {
-        elements.add(when (p.nextToken()) {
-          END_ARRAY -> return@loop
-          START_OBJECT -> JsonObjectDeserializer(p, context)
-          START_ARRAY -> invoke(p, context)
-          VALUE_EMBEDDED_OBJECT -> JsonDeserializer.fromEmbedded(p, context)
-          VALUE_STRING -> JsonStringDeserializer(p, context)
-          VALUE_NUMBER_INT -> JsonIntegralDeserializer(p, context)
-          VALUE_NUMBER_FLOAT -> JsonFractionalDeserializer(p, context)
-          VALUE_TRUE -> JsonTrue
-          VALUE_FALSE -> JsonFalse
-          VALUE_NULL -> JsonNull
-          else -> throw reportWrongTokenException(context, END_ARRAY)
-        })
-      }
+    loop@while (true) {
+      elements.add(when (p.nextToken()) {
+        END_ARRAY -> break@loop
+        START_OBJECT -> JsonObjectDeserializer(p, context)
+        START_ARRAY -> invoke(p, context)
+        VALUE_EMBEDDED_OBJECT -> JsonDeserializer.fromEmbedded(p, context)
+        VALUE_STRING -> JsonStringDeserializer(p, context)
+        VALUE_NUMBER_INT -> JsonIntegralDeserializer(p, context)
+        VALUE_NUMBER_FLOAT -> JsonFractionalDeserializer(p, context)
+        VALUE_TRUE -> JsonTrue
+        VALUE_FALSE -> JsonFalse
+        VALUE_NULL -> JsonNull
+        else -> throw reportWrongTokenException(context, END_ARRAY)
+      })
     }
     return JsonArray(elements.build())
   }
--- src/main/kotlin/io/github/portfoligno/json/ast/codec/JsonObjectDeserializer.kt
+++ src/main/kotlin/io/github/portfoligno/json/ast/codec/JsonObjectDeserializer.kt
@@ -13,27 +13,25 @@ object JsonObjectDeserializer : ExpectedTokenDeserializer<JsonObject>(START_OBJE
   fun invoke(p: JsonParser, context: DeserializationContext): JsonObject {
     val elements = ImmutableMap.builder<String, Json>()
 
-    apply loop@{
-      while (true) {
-        elements.put(
-            when (p.nextToken()) {
-              FIELD_NAME -> p.currentName
-              END_OBJECT -> return@loop
-              else -> throw reportWrongTokenException(context, JsonToken.END_OBJECT)
-            },
-            when (p.nextToken()) {
-              START_OBJECT -> invoke(p, context)
-              START_ARRAY -> JsonArrayDeserializer(p, context)
-              VALUE_EMBEDDED_OBJECT -> JsonDeserializer.fromEmbedded(p, context)
-              VALUE_STRING -> JsonStringDeserializer(p, context)
-              VALUE_NUMBER_INT -> JsonIntegralDeserializer(p, context)
-              VALUE_NUMBER_FLOAT -> JsonFractionalDeserializer(p, context)
-              VALUE_TRUE -> JsonTrue
-              VALUE_FALSE -> JsonFalse
-              VALUE_NULL -> JsonNull
-              else -> throw reportWrongTokenException(context, JsonToken.VALUE_NULL)
-            })
-      }
+    loop@while (true) {
+      elements.put(
+          when (p.nextToken()) {
+            FIELD_NAME -> p.currentName
+            END_OBJECT -> break@loop
+            else -> throw reportWrongTokenException(context, JsonToken.END_OBJECT)
+          },
+          when (p.nextToken()) {
+            START_OBJECT -> invoke(p, context)
+            START_ARRAY -> JsonArrayDeserializer(p, context)
+            VALUE_EMBEDDED_OBJECT -> JsonDeserializer.fromEmbedded(p, context)
+            VALUE_STRING -> JsonStringDeserializer(p, context)
+            VALUE_NUMBER_INT -> JsonIntegralDeserializer(p, context)
+            VALUE_NUMBER_FLOAT -> JsonFractionalDeserializer(p, context)
+            VALUE_TRUE -> JsonTrue
+            VALUE_FALSE -> JsonFalse
+            VALUE_NULL -> JsonNull
+            else -> throw reportWrongTokenException(context, JsonToken.VALUE_NULL)
+          })
     }
     return JsonObject(elements.build())
   }
